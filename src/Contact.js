17e8656c937f1f8df8296d4a05bea770bf421598
AdityaH1304 <hulleaditya4@gmail.com>
2023-10-15 15:40:40 +0530

    first commit

---
+++ src/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <section id="contact">
+            <h2>Contact Me</h2>
+            <p>Email: hulleaditya4@gmail.com</p>
+            <p>LinkedIn: linkedin.com/in/aditya</p>
+        </section>
+    );
+}
+
+export default Contact;
---
+++ src/Projects.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <section id="projects">
+            <h2>Projects</h2>
+            {/* Add your project details here */}
+        </section>
+    );
+}
+
+export default Projects;
